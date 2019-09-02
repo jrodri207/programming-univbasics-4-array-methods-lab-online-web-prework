31825f8d157c2b7e2ca64b4baaac780e4bd59b20
Joshua Rodriguez <josh030596@gmail.com>
2019-09-02 00:51:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-  using_sort = ["ribs", "chicken", "peas", "meat"]
+  using_sort = ["wow", "chicken", "peas", "meat"]
   using_sort.sort
 end
 
